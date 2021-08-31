f908cff50619a0f5b21691295e4caa443ae51ace
rixanne <rixanne@users.noreply.github.com>
2021-08-30 19:20:28 -0700

    Icons, Favorites, and Comments

---
+++ components/ContactComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+class Contact extends Component {
+
+        static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card title={'Contact Information'}
+                wrapperStyle={{margin: 20}}>
+                    <Text style={{ marginBottom: 2 }}>{"1 Nucamp Way"}</Text>
+                    <Text style={{ marginBottom: 2 }}>{"Seattle, Wa, 98001"}</Text>
+                    <Text style={{ marginBottom: 10 }}>{"U.S.A"}</Text>
+                    <Text style={{ marginBottom: 2 }}>{"Phone: 1-206-555-1234"}</Text>
+                    <Text style={{ marginBottom: 2 }}>{"Email: campsites@nucamp.co"}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+}
+export default Contact;
