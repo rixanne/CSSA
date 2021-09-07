412ae530b998eaeed058016249c80362639e6475
Rixanne <rixanne@users.noreply.github.com>
2021-09-06 18:41:40 -0700

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
-
+import * as Animatable from 'react-native-animatable';
 class Contact extends Component {
   static navigationOptions = {
     title: 'Contact Us'
@@ -10,13 +10,15 @@ class Contact extends Component {
   render() {
     return (
       <ScrollView>
-        <Card title={'Contact Information'} wrapperStyle={{ margin: 20 }}>
-          <Text style={{ marginBottom: 2 }}>{'1 Nucamp Way'}</Text>
-          <Text style={{ marginBottom: 2 }}>{'Seattle, Wa, 98001'}</Text>
-          <Text style={{ marginBottom: 10 }}>{'U.S.A'}</Text>
-          <Text style={{ marginBottom: 2 }}>{'Phone: 1-206-555-1234'}</Text>
-          <Text style={{ marginBottom: 2 }}>{'Email: campsites@nucamp.co'}</Text>
-        </Card>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+          <Card title={'Contact Information'} wrapperStyle={{ margin: 20 }}>
+            <Text style={{ marginBottom: 2 }}>{'1 Nucamp Way'}</Text>
+            <Text style={{ marginBottom: 2 }}>{'Seattle, Wa, 98001'}</Text>
+            <Text style={{ marginBottom: 10 }}>{'U.S.A'}</Text>
+            <Text style={{ marginBottom: 2 }}>{'Phone: 1-206-555-1234'}</Text>
+            <Text style={{ marginBottom: 2 }}>{'Email: campsites@nucamp.co'}</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
