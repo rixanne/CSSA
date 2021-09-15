33af4f1925379c9750029a71dd9299569a5282ea
Rixanne <rixanne@users.noreply.github.com>
2021-09-15 11:03:42 -0700

    Local Notifications

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 class Contact extends Component {
   static navigationOptions = {
@@ -10,7 +10,7 @@ class Contact extends Component {
   render() {
     return (
       <ScrollView>
-        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
           <Card title={'Contact Information'} wrapperStyle={{ margin: 20 }}>
             <Text style={{ marginBottom: 2 }}>{'1 Nucamp Way'}</Text>
             <Text style={{ marginBottom: 2 }}>{'Seattle, Wa, 98001'}</Text>
