67bb5005fd1e132379d2d69455e24ae6abe6c2d3
Rixanne <rixanne@users.noreply.github.com>
2021-09-17 11:26:29 -0700

    Week 4 Workshop

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -5,6 +5,7 @@ import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import * as ImageManipulator from 'expo-image-manipulator';
+import * as MediaLibrary from 'expo-media-library';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -165,7 +166,7 @@ class RegisterTab extends Component {
         console.log(capturedImage);
         this.processImage(capturedImage.uri);
       }
-      MediaLibrary.createAssetAsync(capturedImage.uri);
+      MediaLibrary.saveToLibraryAsync(capturedImage.uri);
     }
   };
 
